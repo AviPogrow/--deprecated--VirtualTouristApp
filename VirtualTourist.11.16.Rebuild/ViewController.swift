2ffbb074bb08bc355380febc25cb9f938f6f58d3
Avi Pogrow <rpogrow@gmail.com>
2015-11-16 14:55:38 -0500

    Initial Commit

---
+++ VirtualTourist.11.16.Rebuild/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist.11.16.Rebuild
+//
+//  Created by new on 11/16/15.
+//  Copyright © 2015 Avi Pogrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
